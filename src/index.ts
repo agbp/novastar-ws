487db96b25d8048c3e85db75c52feab54e14d597
Andrey Golovin <ag.expert.it@gmail.com>
2022-08-10 11:06:32 +0300

    error handling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import serial from '@novastar/serial';
-import { Request, DeviceType } from '@novastar/codec';
+// import { Request, DeviceType } from '@novastar/codec';
 import express from 'express';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
 // const [port] = await serial.findSendingCards();
 // const session = await serial.open(port.path);
@@ -15,38 +16,63 @@ require('dotenv').config();
 // // Close all serial sessions
 // serial.release();
 
-const test = serial.default;
-const test2 = await test.findSendingCards();
+const novastarSerial = serial.default;
+const novastarCardsList = await novastarSerial.findSendingCards();
+const TEST_MODE = Boolean(process.env.TEST_MODE);
+
+interface SendingCard {
+	COM: number,
+	DVI: boolean,
+	Port1: boolean,
+	Port2: boolean,
+	Error: boolean,
+}
+
+interface NovastarRes {
+	Error: boolean,
+	ErrorDescription: string,
+	SendingCards: SendingCard[],
+}
+
+function getNovastarData(): NovastarRes {
+	const novastarRes: NovastarRes = {
+		Error: false,
+		ErrorDescription: '',
+		SendingCards: [],
+	};
+
+	if (novastarCardsList.length <= 0) {
+		novastarRes.Error = true;
+		if (TEST_MODE) {
+			console.log('no novastar cards detected');
+			novastarRes.SendingCards.push({
+				COM: 5,
+				DVI: true,
+				Port1: false,
+				Port2: true,
+				Error: false,
+			});
+			novastarRes.ErrorDescription = 'no novastar cards detected, TEST_MODE is on, test data added';
+		} else {
+			novastarRes.ErrorDescription = 'no novastar cards detected';
+		}
+	}
+	return novastarRes;
+}
 
 const PORT = Number(process.env.PORT) || 5000;
 
 const app = express();
 app.use(express.json());
 app.get('/', async (req, res) => {
-	const novastarData = {
-		Error: false,
-		SendingCards:
-			[
-				{
-					COM: 5,
-					DVI: true,
-					Port1: false,
-					Port2: true,
-					Error: false,
-				},
-			],
-	};
-	return res.status(200).json(novastarData);
+	const nsRes = getNovastarData();
+	return res.status(200).json(nsRes);
 });
+
 try {
 	app.listen(PORT, () => {
 		console.log(`Server started on port ${PORT}`);
 	});
-	if (!process.env.TIN_INVEST_API2_PRODUCTION_URL) {
-		throw new Error('env.TIN_INVEST_API2_PRODUCTION_URL not defined');
-	}
 } catch (e) {
 	console.log(e);
 }
-
-console.log(test2);
